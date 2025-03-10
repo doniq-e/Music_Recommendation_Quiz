9cf51b7b1bb1e509fd2900fa919ec9b6d6dd7999
Donique South <donique.south@ucalgary.ca>
2025-03-10 15:04:07 -0600

    Intro message :)

---
+++ music_rec.py
@@ -0,0 +1,2 @@
+print("Welcome to the music recommendation questionare!\nWe will ask you a few questions about your music interests.\nThen, we will reccomend music genres and artists we think you will like!\n")
+
